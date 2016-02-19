6b2cb903c681b188240e50e249a84acadcbed897
ngjaying <ngjaying@gmail.com>
2016-02-19 15:23:27 +0800

    Add monitor
    Add email sender

--- debug.js
+++ debug.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var executor = require('./lib/executor');
-
+var monitor = require('./lib/monitor');
+console.log('start test');
 // executor.compare({
 // 	url: 'url2',
 // 	jqpath: 'jqpath2',
@@ -27,12 +28,21 @@ var executor = require('./lib/executor');
 // }, function(err, value) {
 // 	console.log('no change');
 // });
-console.log('start to run');
-executor.runInSchedule({
-	'url': 'http://www.xmws.gov.cn/sydwzk/policy/policy.jsp?TypeID=7',
-	'jqpath': 'form[name=formRight] table:nth-of-type(5) td a'
-}, function(err, value) {
-	console.log('changed to ' + value);
-}, function(err, value) {
-	console.log('no change');
+// executor.runInSchedule({
+// 	'url': 'http://www.xmws.gov.cn/sydwzk/policy/policy.jsp?TypeID=7',
+// 	'jqpath': 'form[name=formRight] table:nth-of-type(5) td a'
+// }, function(err, value) {
+// 	console.log('changed to ' + value);
+// }, function(err, value) {
+// 	console.log('no change');
+// });
+
+monitor.sendEmail({
+	notifiers: ['ngjaying@gmail.com', 'johnnyyellow@gmail.com'],
+	blockname: 'xiamenweisheng',
+	value: 'new value'
+}, function(err, info) {
+	if (err)
+		return console.log(err);
+	console.log('end test');
 });
